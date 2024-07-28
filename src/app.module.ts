ce7fe3738627f7a7523ca222310cbbcd6f79b4e1
Ákos Beke <akosbeke@gmail.com>
2024-07-28 09:59:36 +0200

    added adminjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    // AdminJS version 7 is ESM-only. In order to import it, you have to use dynamic imports.
+    import('@adminjs/nestjs').then(({ AdminModule }) =>
+      AdminModule.createAdminAsync({
+        useFactory: () => ({
+          adminJsOptions: {
+            rootPath: '/admin',
+            resources: [],
+          },
+        }),
+      }),
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
